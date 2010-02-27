ee37719dd6064342621bf9ea32938e1a0750af7b
arron.woods <arron.woods@123dae08-1fcc-11df-980b-c1ecf858a722>
2010-02-27 13:05:21 +0000

    Database types

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -3,7 +3,17 @@
 class IndexController extends Zend_Controller_Action
 {
     public function indexAction()
-    {}
+    {
+			$types = array(
+				'PDO_MYSQL' => 'MySQL (PDO)',
+				'MYSQLI' => 'MySQLi',
+				'PDO_MSSQL' => 'MsSQL [untested]',
+				'PDO_SQLITE' => 'Sql Lite [untested]',
+				'ORACLE' => 'Oracle',
+			);
+			
+			$this->view->types = $types;
+	}
 
 
 }

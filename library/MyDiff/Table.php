caa69baf92b0da63026848a598baa4c95d6deeab
arron.woods <arron.woods@123dae08-1fcc-11df-980b-c1ecf858a722>
2011-03-15 18:40:59 +0000

    Start of tests on Comparison operations

--- library/MyDiff/Table.php
+++ library/MyDiff/Table.php
@@ -13,17 +13,24 @@ class MyDiff_Table extends MyDiff_Item {
     public function __construct($db, $tableName) {
         $this->name = $tableName;
         $this->_database = $db;
-        $this->_table = new MyDiff_Db_Table(array('name' => $tableName, 'db' => $db->getDb()));
+    }
+
+    public function getTable() {
+        if ($this->_table === null) {
+            $this->_table = new MyDiff_Db_Table(array('name' => $this->name, 'db' => $this->_database->getDb()));
+        }
+
+        return $this->_table;
     }
 
     /**
      * Get list of columns and their metadata
      */
     public function getColumns() {
-        if($this->_columns === null) {
+        if ($this->_columns === null) {
             $columns = array();
-            $metadata = $this->_table->info(Zend_Db_Table_Abstract::METADATA);
-            foreach($metadata AS $columnName => $columnMetaData) {
+            $metadata = $this->getTable()->info(Zend_Db_Table_Abstract::METADATA);
+            foreach ($metadata AS $columnName => $columnMetaData) {
                 $columns[$columnName] = new MyDiff_Table_Column($columnMetaData);
             }
 
@@ -38,8 +45,9 @@ class MyDiff_Table extends MyDiff_Item {
      * Get array of primary keys (column names)
      */
     public function getPrimaryColumns() {
-        if($this->_primaryColumns === null && !$this->_table->noPrimaryKey)
-            $this->_primaryColumns = $this->_table->info(Zend_Db_Table_Abstract::PRIMARY);
+        if ($this->_primaryColumns === null && !$this->getTable()->noPrimaryKey) {
+            $this->_primaryColumns = $this->getTable()->info(Zend_Db_Table_Abstract::PRIMARY);
+        }
 
         return $this->_primaryColumns;
     }
@@ -48,28 +56,27 @@ class MyDiff_Table extends MyDiff_Item {
      * Get the table's mysql storage engine type
      */
     public function getEngine() {
-        if($this->_engine === null) {
-            $select = $this->_table->select();
+        if ($this->_engine === null) {
+            $select = $this->getTable()->select();
             $select->setIntegrityCheck(false);
             $select->from('information_schema.TABLES', 'ENGINE')
                     ->where('TABLE_SCHEMA = ?', $this->_database->name)
                     ->where('TABLE_NAME = ?', $this->name);
 
 
-            $row = $this->_table->fetchRow($select);
-            $this->_engine = isset($row['ENGINE'])? $row['ENGINE'] : null;
+            $row = $this->getTable()->fetchRow($select);
+            $this->_engine = isset($row['ENGINE']) ? $row['ENGINE'] : null;
         }
 
         return $this->_engine;
     }
 
     public function getChecksum() {
-        if($this->getEngine() == 'MyISAM') {
+        if ($this->getEngine() == 'MyISAM') {
             $stmt = $this->_database->getDb()->query('CHECKSUM TABLE `' . $this->name . '`');
             $result = $stmt->fetch();
-            return isset($result['Checksum'])? $result['Checksum'] : null;
-        }
-        else {
+            return isset($result['Checksum']) ? $result['Checksum'] : null;
+        } else {
             return null;
         }
     }
@@ -78,18 +85,18 @@ class MyDiff_Table extends MyDiff_Item {
      * Get the table rows (i.e. data)
      */
     public function getRows() {
-        if($this->_rows === null) {
+        if ($this->_rows === null) {
             $rows = array();
-            $dbRows = $this->_table->fetchAll()->toArray();
-            foreach($dbRows AS $dbRow) {
+            $dbRows = $this->getTable()->fetchAll()->toArray();
+            foreach ($dbRows AS $dbRow) {
                 $row = new MyDiff_Table_Row($this, $dbRow);
 
                 // check uid is unique
                 // useful for data comparisons where we might have duplicates not using primary keys
                 $i = 0;
                 $keys = array_keys($rows);
-                while(in_array($row->uid, $keys)) {
-                    if($i > 1000)
+                while (in_array($row->uid, $keys)) {
+                    if ($i > 1000)
                         throw new Exception('Found over 1000 duplicate rows');
                     $row->uid = md5($row->uid . $i);
                     $i++;
@@ -110,8 +117,8 @@ class MyDiff_Table extends MyDiff_Item {
      * Remove data from rows that don't have any differences
      */
     public function pruneRows() {
-        foreach($this->getRows() AS $rowId => $row) {
-            if(!$row->hasDiffs())
+        foreach ($this->getRows() AS $rowId => $row) {
+            if (!$row->hasDiffs())
                 unset($this->_rows[$rowId]);
         }
     }
@@ -119,15 +126,8 @@ class MyDiff_Table extends MyDiff_Item {
     /**
      * Set rows to blank, because diff now required
      */
-    public function blankRows(){
+    public function blankRows() {
         $this->_rows = array();
-    }
-
-    /**
-     * Get the db table instance
-     */
-    public function getTable() {
-        return $this->_table;
     }
 
 }
